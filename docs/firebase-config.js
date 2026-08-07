ddf83fe2cebfe6db453393c0e1fff2b2fe736804
DenisT <trunden02@gmail.com>
2026-08-07 20:26:06 +0300

    Авто-обновление: 2026-08-07 20:26

---
+++ docs/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-qPsseowxneaW8h2usDhpEnBYWyeO1fM",
+  authDomain: "oldpictureart.firebaseapp.com",
+  projectId: "oldpictureart",
+  storageBucket: "oldpictureart.firebasestorage.app",
+  messagingSenderId: "98669471826",
+  appId: "1:98669471826:web:fb15ae51e40013da32978b",
+  measurementId: "G-F7FDZ15CEV"
+};
